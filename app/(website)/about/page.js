64dc8db9f852c592f4e0f8c9cd96b490c3df5519
Martí Arnau <marnauortega@gmail.com>
2023-09-16 16:39:00 +0200

    test(image loads as webp?)

--- app/(website)/about/page.js
+++ app/(website)/about/page.js
@@ -21,6 +21,7 @@ export default async function About() {
       <Link href="/">Go to Home</Link>
       <div className="text">{title}</div>
       <div className="text">{Math.random()}</div>
+      <Image src="/poster.jpeg" width="1200" height="1074" alt="some image" />
     </div>
   );
 }
